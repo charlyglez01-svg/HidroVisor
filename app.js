060086eee900a3be53a0a968e1200f02936c3f23
charlyglez01-svg <charlyglez01@gmail.com>
2026-06-03 10:37:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -59,4 +59,9 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(Servidor escuchando en el puerto ${PORT});
+});
+
 module.exports = app;
